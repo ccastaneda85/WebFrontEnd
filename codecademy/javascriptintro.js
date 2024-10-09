53e746b03dd3f9eb9ac1c24055183f81b57dd6d4
Joshua Castasneda <josh@Joshuas-MacBook-Pro-4.local>
2024-10-09 12:27:40 -0700

    stuff

--- codecademy/javascriptintro.js
+++ codecademy/javascriptintro.js
@@ -1,74 +1,76 @@
+// console.log("Hello World");
+// // alert("shut it down!");
+// console.log("Hello World", 4 + 6, "Another log");
+// console.warn("this is warning");
+// console.error("This is an error");
+// const a = 10;
+// console.log(a);
+
+
+// let b;
+// var c;
+// console.log(a, b, c);
+
+// let str1='hello'
+// console.log(str1);
+// d = true;
+// let f;
+// let e = null;
+// console.log(d, typeof(d), e, typeof(e), f, typeof(f));
+
+// // Objects
+// var marks = {
+//     ravi: 34,
+//     shubham: 78,
+//     harry: 99.977
+// }
+// console.log(marks);
+
+// age = 25;
+// // if-else Ladder
+// if(age > 32){
+//     console.log("You are not a kid");
+// }
+// else if(age >26){
+//     console.log("Bachhe nahi rahe");
+// }
+// else if(age >22){
+//     console.log("Yes Bachhe nahi rahe");
+// }
+// else if(age >18){
+//     console.log("18 Bachhe nahi rahe");
+// }
+// else{
+//     console.log("Bachhe rahe");
+// }
+// console.log("End of ladder");
+
+
+// var arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(arr);
+// for(var i=0;i<arr.length;i++){
+//     if(i==2){
+//         // break;
+//         continue;
+//     }
+//     console.log(arr[i])
+// }
+
+
+// /*
+// Primitives:
+// strings
+// boolean
+// null
+// undefined
+// Symbol
+// Bigint
+
+// Reference Types:
+// Arrays,
+// Object Literals
+// Functions
+
+//  */
+
 console.log("Hello World");
-// alert("shut it down!");
-console.log("Hello World", 4 + 6, "Another log");
-console.warn("this is warning");
-console.error("This is an error");
-const a = 10;
-console.log(a);
-
-
-let b;
-var c;
-console.log(a, b, c);
-
-let str1='hello'
-console.log(str1);
-d = true;
-let f;
-let e = null;
-console.log(d, typeof(d), e, typeof(e), f, typeof(f));
-
-// Objects
-var marks = {
-    ravi: 34,
-    shubham: 78,
-    harry: 99.977
-}
-console.log(marks);
-
-age = 25;
-// if-else Ladder
-if(age > 32){
-    console.log("You are not a kid");
-}
-else if(age >26){
-    console.log("Bachhe nahi rahe");
-}
-else if(age >22){
-    console.log("Yes Bachhe nahi rahe");
-}
-else if(age >18){
-    console.log("18 Bachhe nahi rahe");
-}
-else{
-    console.log("Bachhe rahe");
-}
-console.log("End of ladder");
-
-
-var arr = [1, 2, 3, 4, 5, 6, 7];
-console.log(arr);
-for(var i=0;i<arr.length;i++){
-    if(i==2){
-        // break;
-        continue;
-    }
-    console.log(arr[i])
-}
-
-
-/*
-Primitives:
-strings
-boolean
-null
-undefined
-Symbol
-Bigint
-
-Reference Types:
-Arrays,
-Object Literals
-Functions
-
- */
